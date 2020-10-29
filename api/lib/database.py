19271e00525cb96cecb5d94f2502cdce8ebfe053
Brian <brian@brianjuhl.com>
2020-10-29 14:56:04 -0500

    Return connection from function

--- api/lib/database.py
+++ api/lib/database.py
@@ -2,10 +2,12 @@ import os
 from mongoengine import connect, disconnect, errors
 
 
-def connect_to_mongo_service():
-    """Connect to mongo service by URI exposed by docker network"""
-    mongo_uri = os.environ["MONGO_URI"]
-    connect("rx_code_challenge_pharmacies", host=mongo_uri)
+def connect_to_mongo_service(mongo_uri=os.environ["MONGO_URI"]):
+    """ 
+    Connect to mongo uri explicitly provided or by default, 
+    the URI exposed by docker network
+    """
+    return connect("rx_code_challenge_pharmacies", host=mongo_uri)
 
 
 def disconnect_from_mongo_service():
