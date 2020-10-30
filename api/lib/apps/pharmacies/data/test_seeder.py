989c30c11b46a2ecb654c103ed345e3a6949c08d
Brian <brian@brianjuhl.com>
2020-10-29 19:08:21 -0500

    Add tests

--- api/lib/apps/pharmacies/data/test_seeder.py
+++ api/lib/apps/pharmacies/data/test_seeder.py
@@ -3,7 +3,8 @@ import pathlib
 import csv
 
 from ....database import connect_to_mongo_service, disconnect_from_mongo_service
-from .seeder import parse_pharmacies_from_csv
+from .seeder import parse_pharmacies_from_csv, seed_pharmacies
+from ..models import Pharmacy
 
 
 class TestPharmacySeeder(unittest.TestCase):
@@ -11,21 +12,31 @@ class TestPharmacySeeder(unittest.TestCase):
     def setUp(self):
         """set up test database connection"""
         connect_to_mongo_service("mongomock://localhost")
+        # assuming file is in the same directory as this file
+        module_path = pathlib.Path(__file__).parent.absolute()
+        self.pharmacy_data = parse_pharmacies_from_csv(
+            f"{module_path}/pharmacies.csv")
 
     def tearDown(self):
         """disconnect test database"""
+        del self.pharmacy_data
         disconnect_from_mongo_service()
 
-    def parse_pharmacies_from_csv(self):
+    def test_parse_pharmacies_from_csv(self):
         """successfully parses csv provided in code challenge"""
-        # assuming file is in the same directory as this file
-        module_path = pathlib.Path(__file__).parent.absolute()
-        test_pharmacy = parse_pharmacies_from_csv(f"{module_path}/pharmacies.csv")[0]
-        self.assertIsInstance(test_pharmacy["name"], str)
-        self.assertIsInstance(test_pharmacy["address"], str)
-        self.assertIsInstance(test_pharmacy["city"], str)
-        self.assertIsInstance(test_pharmacy["state"], str)
-        self.assertIsInstance(test_pharmacy["zip_code"], int)
-        self.assertIsInstance(test_pharmacy["latitude"], float)
-        self.assertIsInstance(test_pharmacy["longitude"], float)
+        for pharmacy in self.pharmacy_data:
+            self.assertIsNotNone(pharmacy["name"])
+            self.assertIsNotNone(pharmacy["address"])
+            self.assertIsNotNone(pharmacy["city"])
+            self.assertIsNotNone(pharmacy["state"])
+            self.assertIsNotNone(pharmacy["zip_code"])
+            self.assertIsNotNone(pharmacy["latitude"])
+            self.assertIsNotNone(pharmacy["longitude"])
 
+    def test_seed_pharmacies(self):
+        """successfully saves parsed pharmacy data"""
+        # will raise vailidation error if pharmacy has invalid shape
+        seed_pharmacies(self.pharmacy_data)
+        self.assertGreater(len(Pharmacy.objects), 1)
+
+        
