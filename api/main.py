a2545a9d26e8ece793517931a5f373628d733382
Brian <brian@brianjuhl.com>
2020-10-30 09:06:50 -0500

    Import and include pharmacies router

--- api/main.py
+++ api/main.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
 
+from lib.apps.pharmacies.router import router as pharmacies_router
+
 app = FastAPI()
 
+# register pharmacies app routes
+app.include_router(
+    pharmacies_router,
+    prefix="/pharmacies",
+    tags=["pharmacies"],
+)
